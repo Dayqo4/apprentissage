a9ab6d2e1ba2ea5640d8a1b1d11b33abd1c25669
jjj buiness <jjjbuiness@MacBook-Pro-de-jjj.local>
2026-04-23 11:42:18 +0200

    new skill

--- appr.js
+++ appr.js
@@ -1,9 +1,26 @@
 OK enfin voila un fichier que je peux modifier 
-vive la république 
 
 pour lire le contenu de mon dossier je fais ls 
 pour lire le contenu de ce fichier depuis mon dossier je fais cat nom de mon fichier 
 pour modifier mon fichier je fais nano nomdemonfichier
 pour créer une dossier je fais mkdir nomdemondossier
-& voilà 
+
+pour versionner avec github: 
+
+git init --> initialise le repository local 
+
+git remote -v --> Connaitre l'état de mon repository (branché ou non au distant)
+
+git status --> connaitre le statut du repository
+
+git add . --> Ajouter tous les dossiers en stagging
+
+git commit -m "contenu de ma modification" --> Décrire le changement du fichier 
+
+git push --> pousser mon changement local sur le dépot distant 
+
+git checkout -b nomdemabranche --> créationd d'une nouvelle branche
+
+
+& voilà 23.04.26
 
