c1b1ed980d72b3aa74a7fee50b0dde4ce3bbdd53
jjj buiness <jjjbuiness@MacBook-Pro-de-jjj.local>
2026-04-23 11:15:54 +0200

    first commit

---
+++ appr.js
@@ -0,0 +1,9 @@
+OK enfin voila un fichier que je peux modifier 
+vive la république 
+
+pour lire le contenu de mon dossier je fais ls 
+pour lire le contenu de ce fichier depuis mon dossier je fais cat nom de mon fichier 
+pour modifier mon fichier je fais nano nomdemonfichier
+pour créer une dossier je fais mkdir nomdemondossier
+& voilà 
+
